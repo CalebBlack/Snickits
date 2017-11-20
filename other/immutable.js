b40c5acf6db8598f847a43eee10afd496f022d14
Caleb Black <caleb.paul.black@gmail.com>
2017-11-20 07:55:03 -0700

    make immutable proxy handle other methods

--- other/immutable.js
+++ other/immutable.js
@@ -2,5 +2,5 @@
 function immutable(object={},clone=false){
   if (typeof object !== 'object') throw new Error('Cannot Create Immutable, Input not Object!');
   if (clone === true) object = Object.assign({},object);
-  return new Proxy(object,{get:(target,name)=>{return object[name]},set:()=>{}});
+  return new Proxy(object,{get:(target,name)=>{return object[name]},set:()=>{return false;},deleteProperty:()=>{return false;},defineProperty:()=>{return false;},setPrototypeOf:()=>{return false;}});
 }
