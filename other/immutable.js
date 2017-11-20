91f814e9f5d78f8691eb8372e5e439574baf103b
Caleb Black <caleb.paul.black@gmail.com>
2017-11-20 07:46:56 -0700

    add parameter to make clone in immutable

--- other/immutable.js
+++ other/immutable.js
@@ -1,5 +1,6 @@
 // Simply converts object into an immutable
-function immutable(object={}){
+function immutable(object={},clone=false){
   if (typeof object !== 'object') throw new Error('Cannot Create Immutable, Input not Object!');
+  if (clone === true) object = Object.assign({},object);
   return new Proxy(object,{get:(target,name)=>{return object[name]},set:()=>{}});
 }
